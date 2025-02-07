0bcbf10547082ec744690893fe3c46f135ba433a
bananas24 <el8dan@gmail.com>
2025-02-07 21:27:08 +0100

    update neovim path

--- src/neovim/helpers/installers/ubuntu-00.00-50.00.sh
+++ src/neovim/helpers/installers/ubuntu-00.00-50.00.sh
@@ -8,26 +8,26 @@ cd "$TEMP_DIR"
 apt update
 apt install -y wget
 
-wget "https://github.com/neovim/neovim/releases/download/$NVIM_VERSION/nvim-linux64.tar.gz"
-tar xf nvim-linux64.tar.gz
-rm nvim-linux64.tar.gz
+wget "https://github.com/neovim/neovim/releases/download/$NVIM_VERSION/nvim-linux-x86_64.tar.gz"
+tar xf nvim-linux-x86_64.tar.gz
+rm nvim-linux-x86_64.tar.gz
 
 # move the executable
-mv --force nvim-linux64/bin/nvim /usr/local/bin
+mv --force nvim-linux-x86_64/bin/nvim /usr/local/bin
 
 # --symbolic & --force flags does not exist in alpine & busybox
 ln -s -f /usr/local/bin/nvim /usr/bin
 
 # copy share files
-cp --recursive --update --verbose nvim-linux64/share/* /usr/local/share
-rm -rf nvim-linux64/share
+cp --recursive --update --verbose nvim-linux-x86_64/share/* /usr/local/share
+rm -rf nvim-linux-x86_64/share
 
 # copy libs
-cp --recursive --update --verbose nvim-linux64/lib/* /usr/local/lib
-rm -rf nvim-linux64/lib
+cp --recursive --update --verbose nvim-linux-x86_64/lib/* /usr/local/lib
+rm -rf nvim-linux-x86_64/lib
 
 # copy man pages
-if [ -d nvim-linux64/man ]; then
-  cp --recursive --update --verbose nvim-linux64/man/* /usr/local/man
-  rm -rf nvim-linux64/man
+if [ -d nvim-linux-x86_64/man ]; then
+  cp --recursive --update --verbose nvim-linux-x86_64/man/* /usr/local/man
+  rm -rf nvim-linux-x86_64/man
 fi
